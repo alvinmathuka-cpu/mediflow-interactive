3e3634e1ee367a458d34d897becb53b6fd2ef446
MadBunny <frankwamuyu40@gmail.com>
2026-08-12 15:33:53 +0300

    form page & its styling (#1)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LoginForm from "./Login page/LoginForm";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <LoginForm />
   </StrictMode>,
-)
+);
